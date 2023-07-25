86971ad94ca35e9992b5eb022bde0ea268b53d7b
SergeyFullstack <111132907+SergeyFullstack@users.noreply.github.com>
2023-07-25 22:01:05 +0300

    create hw-01

---
+++ contacts.js
@@ -0,0 +1,50 @@
+const fs = require("fs/promises");
+
+const path = require("path");
+
+const contactPath = path.join(__dirname, "./db/contacts.json");
+
+const { nanoid } = require("nanoid");
+
+const listContacts = async () => {
+  const data = await fs.readFile(contactPath);
+  console.log(data);
+  return JSON.parse(data);
+};
+
+const getContactById = async (contactId) => {
+  const allContacts = await listContacts();
+  const findContact = allContacts.find((el) => el.id === contactId);
+  return findContact;
+};
+
+const removeContact = async (contactId) => {
+  const allContacts = await listContacts();
+  const index = allContacts.findIndex((el) => el.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = allContacts.splice(index, 1);
+  await fs.writeFile(contactPath, JSON.stringify(allContacts, null, 2));
+  return result;
+};
+
+const addContact = async (name, email, phone) => {
+  const allContacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  allContacts.push(newContact);
+  await fs.writeFile(contactPath, JSON.stringify(allContacts, null, 2));
+  return newContact;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+};
